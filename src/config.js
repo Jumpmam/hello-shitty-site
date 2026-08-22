ae8c5a95a3152bafb567c248458b5fdf2042a272
Jumpmam <pacman228pac@gmail.com>
2026-08-22 08:28:06 +0300

    chore: update CA and lowercase the ticker to $shitty

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // Public config. Flip these when the token is live.
 export const CONFIG = {
-  ticker: "$Shitty",
-  ca: "3cLPHQfNQYdqZnDuZxHMHYLUXPnUPW3a2FZXavS2pump",
-  pump: "https://pump.fun/coin/3cLPHQfNQYdqZnDuZxHMHYLUXPnUPW3a2FZXavS2pump",
+  ticker: "$shitty",
+  ca: "9PTyzTApVhxSevmahZzgCpWFzoxKUXWAt4aV8311pump",
+  pump: "https://pump.fun/coin/9PTyzTApVhxSevmahZzgCpWFzoxKUXWAt4aV8311pump",
   x: "https://x.com/HelloShitty_Fun",
   dexscreener: "",
   url: "https://hello-shitty.vercel.app",
