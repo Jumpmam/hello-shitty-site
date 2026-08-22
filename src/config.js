4652f06e3782b93432de6e2f4ceef6f7db1abac7
Jumpmam <pacman228pac@gmail.com>
2026-08-22 08:11:12 +0300

    chore: wire up live CA, pump.fun link, and X handle

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 // Public config. Flip these when the token is live.
 export const CONFIG = {
   ticker: "$SHITTY",
-  ca: "SHE_HASNT_LAUNCHED_YET",
-  pump: "",
-  x: "",
+  ca: "3cLPHQfNQYdqZnDuZxHMHYLUXPnUPW3a2FZXavS2pump",
+  pump: "https://pump.fun/coin/3cLPHQfNQYdqZnDuZxHMHYLUXPnUPW3a2FZXavS2pump",
+  x: "https://x.com/HelloShitty_Fun",
   dexscreener: "",
   url: "https://hello-shitty.vercel.app",
 };
